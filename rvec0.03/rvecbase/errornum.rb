85ee7ee8fed34a40581553f4090142a0353cbf3b
Jake Leroux <31752192+Answerious@users.noreply.github.com>
2021-11-30 00:07:04 -0700

    Delete rvec0.03 directory

--- rvec0.03/rvecbase/errornum.rb
+++
@@ -1,7 +0,0 @@
-#Error function for rvec
-#(c) Jake Leroux 2021-22 BSD Class 3
-
-def apt(x, y)
-    @return = [] and y.each{|sublist| @sublist = [] and sublist.each{|item| @sublist << (x - item)}and @return << @sublist}
-    return @return
-end
